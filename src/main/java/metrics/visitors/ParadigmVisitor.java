0e6ffafcea4fa58812d9cab31261efb2f8b3b071
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-15 16:16:01 +0200

    improve paradigm score

--- src/main/java/metrics/visitors/ParadigmVisitor.java
+++ src/main/java/metrics/visitors/ParadigmVisitor.java
@@ -1,15 +1,18 @@
 package metrics.visitors;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.stmt.DoStmt;
-import com.github.javaparser.ast.stmt.ForEachStmt;
-import com.github.javaparser.ast.stmt.ForStmt;
-import com.github.javaparser.ast.stmt.WhileStmt;
+import com.github.javaparser.ast.stmt.*;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.resolution.types.ResolvedReferenceType;
+import com.github.javaparser.resolution.types.ResolvedType;
 
 public class ParadigmVisitor extends VoidVisitorAdapter<Void> {
 
@@ -50,11 +53,53 @@ public class ParadigmVisitor extends VoidVisitorAdapter<Void> {
     public void visit(MethodCallExpr n, Void arg) {
         super.visit(n, arg);
 
-        // TODO: check this list of functions
-        List<String> functionalCalls = Arrays.asList("fold", "map", "filter", "count", "exists", "find");
+        List<String> functionalCalls =
+                Arrays.asList("allMatch", "anyMatch", "count", "filter", "findAny", "findFirst",
+                        "flatMap", "flatMapToDouble", "flatMapToInt", "flatMapToLong", "map",
+                        "mapToDouble", "mapToInt", "mapToLong", "noneMatch", "reduce");
+
         if (functionalCalls.contains(n.getNameAsString())){
             functionalCount++;
         }
+    }
+
+    @Override
+    public void visit(MethodDeclaration n, Void arg){
+        super.visit(n, arg);
+
+        if (isRecursive(n) || isHigherOrder(n)){
+            functionalCount++;
+        }
+    }
+
+    private boolean isRecursive(MethodDeclaration method){
+        if (method.getBody().isEmpty()){
+            return false;
+        }
+
+        RecursionVisitor v = new RecursionVisitor();
+        v.visit(method, method);
+        return v.isRecursive();
+    }
+
+    private boolean isHigherOrder(MethodDeclaration method){
+        List<Type> types = new ArrayList<>();
+        types.add(method.getType());
+        types.addAll(method.getParameters().stream().map(Parameter::getType).toList());
+
+        for (Type t : types){
+            ResolvedType resolvedType = t.resolve();
+            if (resolvedType.isReferenceType()){
+                ResolvedReferenceType referenceType = resolvedType.asReferenceType();
+                if (referenceType.getTypeDeclaration().isPresent()){
+                    if (referenceType.getTypeDeclaration().get().isFunctionalInterface()){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
     public double getScore(){
@@ -64,5 +109,29 @@ public class ParadigmVisitor extends VoidVisitorAdapter<Void> {
         return (double) (functionalCount - imperativeCount) / (functionalCount + imperativeCount);
     }
     
+}
+
+class RecursionVisitor extends VoidVisitorAdapter<MethodDeclaration> {
+    private boolean isRecursive = false;
+
+    @Override
+    public void visit(MethodCallExpr methodCall, MethodDeclaration methodDeclaration) {
+        super.visit(methodCall, methodDeclaration);
+
+        String name = methodDeclaration.getNameAsString();
+        int nrParameters = methodDeclaration.getParameters().size();
+
+        if (methodCall.getNameAsString().equals(name) && methodCall.getArguments().size() == nrParameters){
+            if (methodCall.resolve().toDescriptor().equals(methodDeclaration.resolve().toDescriptor())){
+                isRecursive = true;
+            }
+        }
+
+    }
+
+    public boolean isRecursive(){
+        return isRecursive;
+    }
+
 }
 
