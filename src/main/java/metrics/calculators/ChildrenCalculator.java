1348f0fb9178d479845ec0f7f220b06a15bc2b6a
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-15 17:19:59 +0200

    fix children computation

---
+++ src/main/java/metrics/calculators/ChildrenCalculator.java
@@ -0,0 +1,73 @@
+package metrics.calculators;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+
+import java.util.*;
+
+public class ChildrenCalculator {
+    private static ChildrenCalculator instance;
+    private final List<CompilationUnit> allUnits;
+    private Map<ResolvedReferenceTypeDeclaration, List<CompilationUnit>> children;
+
+    private ChildrenCalculator(List<CompilationUnit> allUnits){
+        this.allUnits = allUnits;
+    }
+
+    public static ChildrenCalculator getInstance(List<CompilationUnit> allUnits){
+        if (instance == null){
+            instance = new ChildrenCalculator(allUnits);
+        }
+        return instance;
+    }
+
+    public List<CompilationUnit> getChildren(ClassOrInterfaceDeclaration classOrInterfaceDeclaration){
+        if (children == null){
+            calculateChildren();
+        }
+
+        ResolvedReferenceTypeDeclaration decl = classOrInterfaceDeclaration.resolve();
+        if (children.containsKey(decl)){
+            return children.get(decl);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    private void calculateChildren(){
+        children = new HashMap<>();
+        for (CompilationUnit cu : allUnits){
+            AncestorVisitor v = new AncestorVisitor();
+            v.visit(cu, null);
+            List<ResolvedReferenceTypeDeclaration> ancestors = v.getAncestors();
+
+            for (ResolvedReferenceTypeDeclaration ancestor : ancestors){
+                if (children.containsKey(ancestor)){
+                    children.get(ancestor).add(cu);
+                } else {
+                    List<CompilationUnit> l = new ArrayList<>();
+                    l.add(cu);
+                    children.put(ancestor, l);
+                }
+            }
+        }
+
+    }
+}
+
+class AncestorVisitor extends VoidVisitorAdapter<Void> {
+    private final List<ResolvedReferenceTypeDeclaration> ancestors = new ArrayList<>();
+
+    @Override
+    public void visit(ClassOrInterfaceDeclaration classOrInterface, Void arg) {
+        super.visit(classOrInterface, arg);
+        ancestors.addAll(classOrInterface.resolve().getAncestors(true).
+                stream().map(a -> a.getTypeDeclaration().orElseThrow()).toList());
+    }
+
+    public List<ResolvedReferenceTypeDeclaration> getAncestors() {
+        return ancestors;
+    }
+}
--- src/main/java/metrics/visitors/ChildrenVisitor.java
+++ src/main/java/metrics/visitors/ChildrenVisitor.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.declarations.ResolvedReferenceTypeDeclaration;
+import metrics.calculators.ChildrenCalculator;
 
 public class ChildrenVisitor extends VoidVisitorAdapter<Void> {
     private int numberOfChildren = 0;
@@ -19,18 +19,8 @@ public class ChildrenVisitor extends VoidVisitorAdapter<Void> {
     public void visit(ClassOrInterfaceDeclaration classOrInterface, Void arg) {
         super.visit(classOrInterface, arg);
 
-        ResolvedReferenceTypeDeclaration currentClass = classOrInterface.resolve();
-
-        for (CompilationUnit cu : allUnits){
-            cu.findAll(ClassOrInterfaceDeclaration.class).forEach(classOrInterfaceDeclaration -> {
-                List<ResolvedReferenceTypeDeclaration> ancestors = classOrInterfaceDeclaration.resolve().getAncestors(true).
-                                                                    stream().map(a -> a.getTypeDeclaration().orElseThrow()).toList();
-                if (ancestors.contains(currentClass)){
-                    // A class has the current class as one of its direct ancestors
-                    numberOfChildren++;
-                }
-            });
-        }
+        List<CompilationUnit> children = ChildrenCalculator.getInstance(allUnits).getChildren(classOrInterface);
+        numberOfChildren += children.size();
     }
 
     public int getNOC() {
