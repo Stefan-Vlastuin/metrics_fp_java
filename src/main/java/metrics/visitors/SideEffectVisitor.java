98fc3eca6e69af48011e27fd242944442f4e21ee
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-05-27 10:46:03 +0200

    catch exception

--- src/main/java/metrics/visitors/SideEffectVisitor.java
+++ src/main/java/metrics/visitors/SideEffectVisitor.java
@@ -7,10 +7,13 @@ import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.UnaryExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
+import metrics.ProgressLogger;
+
 import java.util.Arrays;
 
 public class SideEffectVisitor extends VoidVisitorAdapter<Range> {
     private boolean hasSideEffect = false;
+    private final ProgressLogger logger = ProgressLogger.getInstance();
 
     @Override
     public void visit(AssignExpr assignExpr, Range scope) {
@@ -32,11 +35,15 @@ public class SideEffectVisitor extends VoidVisitorAdapter<Range> {
         }
     }
 
-    private boolean hasSideEffect(Expression e, Range scope){
-        if (e instanceof NameExpr nameExpr){
-            ResolvedValueDeclaration decl = nameExpr.resolve();
-            // If the variable is declared inside this scope, it is not a side effect.
-            return !scope.contains(decl.toAst().orElseThrow().getRange().orElseThrow());
+    private boolean hasSideEffect(Expression expr, Range scope){
+        if (expr instanceof NameExpr nameExpr){
+            try {
+                ResolvedValueDeclaration decl = nameExpr.resolve();
+                // If the variable is declared inside this scope, it is not a side effect.
+                return !scope.contains(decl.toAst().orElseThrow().getRange().orElseThrow());
+            } catch (Exception e){
+                logger.log(e, "Could not resolve variable " + nameExpr.getNameAsString() + " at " + nameExpr.getRange());
+            }
         }
         return false;
     }
