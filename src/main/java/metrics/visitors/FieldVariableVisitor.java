c70a90077df10c662b014cf66953c099a2801814
Stefan Vlastuin <stefan.vlastuin@gmail.com>
2024-06-13 11:19:07 +0200

    remove import

--- src/main/java/metrics/visitors/FieldVariableVisitor.java
+++ src/main/java/metrics/visitors/FieldVariableVisitor.java
@@ -4,7 +4,6 @@ import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.UnsolvedSymbolException;
 import com.github.javaparser.resolution.declarations.ResolvedValueDeclaration;
 import metrics.ProgressLogger;
 
